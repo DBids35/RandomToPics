63626417470c87322b88ac52a78fa00825079cdb
Andrew Bidlen <drew.bidlen@yahoo.com>
2017-02-16 14:21:29 -0500

    Very basic image filtering
    Signed-off-by: Andrew Bidlen <drew.bidlen@yahoo.com>

---
+++ imageGeneration.py
@@ -0,0 +1,11 @@
+from pylab import imshow, show, get_cmap
+from numpy import random
+import scipy.ndimage
+Z= random.random((100,100))
+
+for i in range(50):
+    Z=scipy.ndimage.filters.median_filter(Z, size=(2,2))
+
+Z = scipy.ndimage.filters.sobel(Z)
+imshow(Z, cmap=get_cmap("Spectral"), interpolation='nearest')
+show()
