54f9520e9636fbda3a924a16e693d67ee8b37d67
DBids35 <drew.bidlen@yahoo.com>
2017-02-23 10:14:43 -0500

    Successfully generating images via neural network

--- NNImGen.py
+++ NNImGen.py
@@ -1,7 +1,42 @@
 from pylab import imshow, show, get_cmap
 from numpy import random
+import numpy as np
 import scipy.ndimage
-Z= random.random((10,10))
-print(Z[5][5])
-imshow(Z, cmap=get_cmap("Spectral"), interpolation='nearest')
+
+syn0 = 2*np.random.random((10,10)) - 1
+syn1 = 2*np.random.random((10,1)) - 1
+maxX=100
+maxY=100
+
+def spotCheck(Z, x, y):
+    a=x
+    b=y
+    if x<0:
+        a=0
+    elif x>=maxX:
+        a=maxX-1
+
+    if y<0:
+        b=0
+    elif y>=maxY:
+        b=maxY-1
+    
+    return Z[a][b]
+
+
+def neuralNet(Z, x,y, maxX, maxY):
+    X=[x,y, spotCheck(Z, x-1, y-1),spotCheck(Z,x, y-1),spotCheck(Z,x+1, y-1),spotCheck(Z,x+1, y),spotCheck(Z,x+1, y+1), spotCheck(Z,x, y+1),spotCheck(Z,x-1, y+1),spotCheck(Z,x-1, y) ]
+
+    l1 = 1 / (1 + np.exp(-(np.dot(X, syn0))))
+    l2 = 1 / (1 + np.exp(-(np.dot(l1, syn1))))
+    # print(l2[0])
+    return(l2[0])
+
+
+Z= random.random((maxX,maxY))
+Z2=random.random((maxX,maxY))
+for x in range(maxX):
+    for y in range(maxY):
+        Z2[x][y]=neuralNet(Z,x,y,maxX, maxY)
+imshow(Z2, cmap="gray", interpolation='nearest')
 show()
