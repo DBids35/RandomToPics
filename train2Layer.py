be30b04dc5f77c5d3247c3846299815c6cc3bb80
CL-ENS144-15\kevinbridgman <bridgmankevin@gmail.com>
2017-03-02 19:32:00 -0500

    Elimitated the hidden layer in train2layer. Subploted and adjusted the sigmoid function in train3layer

--- train2Layer.py
+++ train2Layer.py
@@ -27,45 +27,33 @@ def nonlin(x, deriv=False):
 np.random.seed(1)
 
 # randomly initialize our weights with mean 0
-syn0 = 2 * random.random((4425,10)) - 1
-syn1 = 2 * random.random((10, 4425)) - 1
+syn0 = 2 * random.random((4425,4425)) - 1
 
-for j in range(200):
+for j in range(500):
 
     # Feed forward through layers 0, 1, and 2
     l0 = X
     l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
 
     # how much did we miss the target value?
-    l2_error = Y - l2
+    l1_error = Y - l1
 
     if (j % 10) == 0:
-        print("Error:" + str(np.mean(np.abs(l2_error))))
+        print("Error:" + str(np.mean(np.abs(l1_error))))
 
     training_coef = 1
-
-    # in what direction is the target value?
-    # were we really sure? if so, don't change too much.
-    l2_delta = l2_error * nonlin(l2, deriv=True)*training_coef
-
-    # how much did each l1 value contribute to the l2 error (according to the weights)?
-    l1_error = l2_delta.dot(syn1.T)
 
     # in what direction is the target l1?
     # were we really sure? if so, don't change too much.
     l1_delta = l1_error * nonlin(l1, deriv=True)*training_coef
 
-    syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
 
 Z=random.random((1,4425))
 
-
 l1 = nonlin(np.dot(Z, syn0))
-l2 = nonlin(np.dot(l1, syn1))
 
-outImage=l2.reshape((75,59))
+outImage=l1.reshape((75,59))
 Z=Z.reshape((75,59))
 plt.subplot(1, 2, 1)
 imshow(Z, cmap="gray", interpolation='nearest')
